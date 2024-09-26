ee373a9e48253aeb310e12214ff3bebe124c9141
LadnerJonas <jonas.ladner+git@tum.de>
2024-09-26 14:51:40 +0200

    cleanup benchmarks by extracting BinaryRelation read from disk

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -15,15 +15,11 @@
 #include "radix/output_data_storage/RadixPartitionManager.hpp"
 #include "radix/worker/process_radix_chunk.hpp"
 
-template<typename PM>
+template<typename T, typename PM>
 void run_morsel_driven_partitioning(
-        const std::string &relation_path, const size_t morsel_size, const size_t num_partitions,
+        const std::shared_ptr<T[]> relation_data, const size_t relation_size, const size_t morsel_size, const size_t num_partitions,
         const int num_threads) {
-    BinaryRelation<int> relation(relation_path);
-    auto relation_data = relation.get_data();
-    auto relation_size = relation.get_size();
-
-    MorselManager<int> global_morsel_manager(relation_data, relation_size, morsel_size);
+    MorselManager<T> global_morsel_manager(relation_data, relation_size, morsel_size);
     PM global_partition_manager(num_partitions);
 
     std::vector<std::thread> threads;
@@ -46,11 +42,8 @@ void run_morsel_driven_partitioning(
 }
 
 template<typename T, size_t num_partitions, size_t batch_size>
-void run_radix_partitioning(const std::string &relation_path,
+void run_radix_partitioning(const std::shared_ptr<T[]> relation_data, const size_t relation_size,
                             const int num_threads) {
-    BinaryRelation<T> relation(relation_path);
-    auto relation_data = relation.get_data();
-    auto relation_size = relation.get_size();
 
     RadixPartitionManager<T, num_partitions> global_partition_manager(num_threads);
 
@@ -87,16 +80,21 @@ void run_radix_partitioning(const std::string &relation_path,
 
 
 constexpr size_t num_partitions = 4;
-constexpr size_t batch_size = 1024;
+constexpr size_t radix_write_out_batch_size = 1024;
 
 int main() {
     bool run_binary_relation_benchmark = false;
-    bool run_sort_based_benchmark = false;
-    bool run_morsel_based_benchmark = false;
+    bool run_sort_based_benchmark = true;
+    bool run_morsel_based_benchmark = true;
     bool run_radix_based_benchmark = true;
 
     auto max_threads = std::thread::hardware_concurrency();
+
     std::string relation_path = "../../input_data/data/relation_int.bin";
+    BinaryRelation<int> relation(relation_path);
+    const auto relation_data = relation.get_data();
+    const auto relation_size = relation.get_size();
+
 
     // Global benchmark parameters
     BenchmarkParameters params;
@@ -119,12 +117,9 @@ int main() {
         std::cout << "Sort based partitioning (in-place)\n";
         {
             params.setParam("impl", "UnsequencedUnstableSortBasedPartitioning");
-            PerfEventBlock e(1'000'000, params, true);
-            BinaryRelation<int> relation(relation_path);
-            auto relation_data = relation.get_data();
-            auto relation_size = relation.get_size();
+            std::vector<int> data = std::vector(relation_data.get(), relation_data.get() + relation_size);
 
-            std::vector<int> data = std::vector<int>(relation_data.get(), relation_data.get() + relation_size);
+            PerfEventBlock e(1'000'000, params, true);
             auto partition_boundaries = sort_partition<int>(data, num_partitions, std::execution::seq);
             auto total_elements = std::accumulate(partition_boundaries.begin(), partition_boundaries.end(), 0,
                                                   [](int sum, const std::pair<int, int> &p) { return sum + p.second - p.first; });
@@ -132,12 +127,9 @@ int main() {
         }
         {
             params.setParam("impl", "ParallelUnstableSortBasedPartitioning   ");
-            PerfEventBlock e(1'000'000, params, false);
-            BinaryRelation<int> relation(relation_path);
-            auto relation_data = relation.get_data();
-            auto relation_size = relation.get_size();
+            std::vector<int> data = std::vector(relation_data.get(), relation_data.get() + relation_size);
 
-            std::vector<int> data = std::vector<int>(relation_data.get(), relation_data.get() + relation_size);
+            PerfEventBlock e(1'000'000, params, false);
             auto partition_boundaries = sort_partition<int>(data, num_partitions, std::execution::par);
             auto total_elements = std::accumulate(partition_boundaries.begin(), partition_boundaries.end(), 0,
                                                   [](int sum, const std::pair<int, int> &p) { return sum + p.second - p.first; });
@@ -148,7 +140,7 @@ int main() {
     if (run_morsel_based_benchmark) {
         std::cout << "\nMorsel-driven partitioning\n";
 
-        constexpr size_t morsel_size = 4096;
+        constexpr size_t morsel_size = 4096 * 100;
         params.setParam("morsel_size", morsel_size);
         // Loop over different thread counts
         for (auto num_threads = 1; num_threads <= max_threads; ++num_threads) {
@@ -160,14 +152,14 @@ int main() {
             {
                 params.setParam("impl", "StdVectorBasedPartitionManager   ");
                 PerfEventBlock e(1'000'000, params, printHeader);
-                run_morsel_driven_partitioning<StdVectorBasedPartitionManager<int>>(
-                        relation_path, morsel_size, num_partitions, num_threads);
+                run_morsel_driven_partitioning<int, StdVectorBasedPartitionManager<int>>(
+                        relation_data, relation_size, morsel_size, num_partitions, num_threads);
             }
             {
                 params.setParam("impl", "LockFreeListBasedPartitionManager");
                 PerfEventBlock e(1'000'000, params, false);
-                run_morsel_driven_partitioning<LockFreeListBasedPartitionManager<int>>(
-                        relation_path, morsel_size, num_partitions, num_threads);
+                run_morsel_driven_partitioning<int, LockFreeListBasedPartitionManager<int>>(
+                        relation_data, relation_size, morsel_size, num_partitions, num_threads);
             }
         }
     }
@@ -175,14 +167,14 @@ int main() {
     if (run_radix_based_benchmark) {
         std::cout << "\nRadix partitioning\n";
         params.setParam("impl", "RadixPartitionManager");
-        params.setParam("write_out_batch", batch_size);
+        params.setParam("write_out_batch", radix_write_out_batch_size);
 
         for (auto num_threads = 1; num_threads <= max_threads; ++num_threads) {
             params.setParam("threads", num_threads);
 
             {
                 PerfEventBlock e(1'000'000, params, true);
-                run_radix_partitioning<int, num_partitions, batch_size>(relation_path, num_threads);
+                run_radix_partitioning<int, num_partitions, radix_write_out_batch_size>(relation_data, relation_size, num_threads);
             }
         }
     }
--- include/radix/worker/process_radix_chunk.hpp
+++ include/radix/worker/process_radix_chunk.hpp
@@ -22,14 +22,13 @@ void process_radix_chunk(T *chunk, size_t chunk_size, RadixPartitionManager<T, n
     for (int i = 0; i < chunk_size; i++) {
         auto partition_id = partition_function(chunk[i], num_partitions);
 
-        if(batch_index[partition_id] == batch_size) {
+        if (batch_index[partition_id] == batch_size) {
             // write out full batch
             auto &[partition_storage, index] = storage_locations[partition_id];
             std::memcpy(&partition_storage[index], batched_storage[partition_id], batch_size * sizeof(T));
             index += batch_size;
             batch_index[partition_id] = 0;
-        }
-        else {
+        } else {
             batched_storage[partition_id][batch_index[partition_id]++] = chunk[i];
         }
 
