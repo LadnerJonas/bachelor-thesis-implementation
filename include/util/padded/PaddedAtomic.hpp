10081a891968bad0a59c064bf2fa0eeea172e122
LadnerJonas <jonas.ladner+git@tum.de>
2024-09-26 14:05:07 +0200

    fix invalid initialization and add fetch_add and get

--- include/util/padded/PaddedAtomic.hpp
+++ include/util/padded/PaddedAtomic.hpp
@@ -8,7 +8,7 @@ template<typename T>
 struct alignas(std::hardware_destructive_interference_size) PaddedAtomic {
     std::atomic<T> value;
 
-    PaddedAtomic() : value(nullptr) {}
+    PaddedAtomic() : value() {}
 
     explicit PaddedAtomic(const T &init) : value(init) {}
 
@@ -18,6 +18,16 @@ struct alignas(std::hardware_destructive_interference_size) PaddedAtomic {
 
     bool compare_exchange_strong(T &expected, T desired, std::memory_order order = std::memory_order_seq_cst) {
         return value.compare_exchange_strong(expected, desired, order);
+    }
+
+    template<typename U = T>
+    typename std::enable_if<std::is_integral<U>::value || std::is_floating_point<U>::value || std::is_pointer<U>::value, U>::type
+    fetch_add(U arg) {
+        return value.fetch_add(arg);
+    }
+
+    T get() {
+        return value.load();
     }
 };
 #endif// PADDEDATOMIC_HPP
