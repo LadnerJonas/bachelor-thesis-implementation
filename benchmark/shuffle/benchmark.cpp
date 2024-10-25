641a4fa4112d4bb797dbaf76e35c6bf344392dca
LadnerJonas <jonas.ladner+git@tum.de>
2024-10-25 21:00:42 +0200

    add RadixSelectiveOrchestrator + benchmarks (slow)

--- benchmark/shuffle/benchmark.cpp
+++ benchmark/shuffle/benchmark.cpp
@@ -1,5 +1,6 @@
 #include "../external/perfevent/PerfEvent.hpp"
 #include "radix/orchestration/RadixOrchestrator.hpp"
+#include "radix/orchestration/RadixSelectiveOrchestrator.hpp"
 #include "tuple-generator/BatchedTupleGenerator.hpp"
 #include "tuple-types/tuple-types.hpp"
 
@@ -7,7 +8,7 @@ template<typename T, unsigned... Partitions>
 void benchmark_RadixOrchestrator() {
     unsigned tuple_count_factor = std::is_same_v<T, Tuple16> ? 5 : 1;
     unsigned tuples_base = 10'000'000;
-    for (unsigned tuples_to_generate = tuples_base * tuple_count_factor; tuples_to_generate <= 2 * tuples_base * tuple_count_factor; tuples_to_generate += tuples_base * tuple_count_factor) {
+    for (unsigned tuples_to_generate = tuples_base * tuple_count_factor; tuples_to_generate <= 1 * tuples_base * tuple_count_factor; tuples_to_generate += tuples_base * tuple_count_factor) {
         auto run_benchmark = [&](auto partition) {
             for (unsigned threads = 1; threads <= std::thread::hardware_concurrency(); threads *= 2) {
                 BenchmarkParameters params;
@@ -44,8 +45,53 @@ void benchmark_RadixOrchestrator() {
     }
 }
 
+template<typename T, unsigned... Partitions>
+void benchmark_RadixSelectiveOrchestrator() {
+    unsigned tuple_count_factor = std::is_same_v<T, Tuple16> ? 5 : 1;
+    unsigned tuples_base = 10'000'000;
+    for (unsigned tuples_to_generate = tuples_base * tuple_count_factor; tuples_to_generate <= 1 * tuples_base * tuple_count_factor; tuples_to_generate += tuples_base * tuple_count_factor) {
+        auto run_benchmark = [&](auto partition) {
+            for (unsigned threads = 1; threads <= std::thread::hardware_concurrency(); threads *= 2) {
+                BenchmarkParameters params;
+                params.setParam("A-Benchmark shuffle", "RadixSelectiveOrchestrator");
+                params.setParam("B-tuple_size", sizeof(T));
+                params.setParam("C-Tuples", tuples_to_generate);
+                double gb_size = static_cast<double>(tuples_to_generate) * sizeof(T) / 1024 / 1024 / 1024;
+                std::ostringstream gb_str;
+                gb_str << std::fixed << std::setprecision(1) << gb_size << " GB";
+                params.setParam("D-GB", gb_str.str());
+                params.setParam("E-Partitions", partition);
+                params.setParam("F-Threads", threads);
+                constexpr unsigned k = 2;
+                params.setParam("G-k", k);
+                PerfEventBlock e(1'000'000, params, tuples_to_generate == tuples_base * tuple_count_factor && threads == 1 && partition == 2);
+
+                RadixSelectiveOrchestrator<T, partition, k> orchestrator(tuples_to_generate, threads);
+                orchestrator.run();
+
+                // Verify the result
+                auto written_tuples = orchestrator.get_written_tuples_per_partition();
+                auto actual_tuples = 0u;
+                for (auto tuples: written_tuples) {
+                    actual_tuples += tuples;
+                }
+
+                if (tuples_to_generate != actual_tuples) {
+                    std::cout << "Test failed: " << actual_tuples << "/" << tuples_to_generate << std::endl;
+                    exit(1);
+                }
+            }
+        };
+
+        // Use fold expression to call run_benchmark with each partition value
+        (run_benchmark(std::integral_constant<unsigned, Partitions>{}), ...);
+    }
+}
+
 int main() {
+    //benchmark_RadixSelectiveOrchestrator<Tuple16, 2, 8, 32, 256, 1024>();
     benchmark_RadixOrchestrator<Tuple16, 2, 8, 32, 256, 1024>();
+    //benchmark_RadixSelectiveOrchestrator<Tuple100, 2, 8, 32, 256, 1024>();
     benchmark_RadixOrchestrator<Tuple100, 2, 8, 32, 256, 1024>();
 
     return 0;
--- include/radix/orchestration/RadixSelectiveOrchestrator.hpp
+++ include/radix/orchestration/RadixSelectiveOrchestrator.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "radix/materialization/ContiniousMaterialization.hpp"
-#include "radix/worker/process_radix_chunk.hpp"
+#include "radix/worker/process_radix_chunk_selectively.hpp"
 #include "slotted-page/page-manager/RadixPageManager.hpp"
 
-template<typename T, size_t partitions>
-class RadixOrchestrator {
+template<typename T, size_t partitions, size_t k>
+class RadixSelectiveOrchestrator {
     ContinuousMaterialization<T> materialization;
     RadixPageManager<T, partitions> page_manager;
     std::vector<std::thread> threads;
@@ -13,7 +13,7 @@ class RadixOrchestrator {
     size_t num_tuples;
 
 public:
-    explicit RadixOrchestrator(size_t num_tuples, size_t num_threads) : materialization(num_tuples), page_manager(num_threads), num_threads(num_threads), num_tuples(num_tuples) {
+    explicit RadixSelectiveOrchestrator(size_t num_tuples, size_t num_threads) : materialization(num_tuples), page_manager(num_threads), num_threads(num_threads), num_tuples(num_tuples) {
     }
 
     void run() {
@@ -30,7 +30,7 @@ public:
             }
             const auto raw_pointer = data.get();
             threads.emplace_back([this, raw_pointer, i, chunk_size]() {
-                process_radix_chunk<T,partitions>(page_manager, raw_pointer + i * chunk_size, chunk_size);
+                process_radix_chunk_selectively<T,partitions, k>(page_manager, raw_pointer + i * chunk_size, chunk_size);
             });
         }
         for (auto &thread: threads) {
