05ec5de7531b55a27045114bf310dc03b1029db5
LadnerJonas <jonas.ladner+git@tum.de>
2024-11-13 14:34:38 +0100

    simplify smb logic

--- include/smb/worker/process_morsel_smb.hpp
+++ include/smb/worker/process_morsel_smb.hpp
@@ -4,25 +4,20 @@
 
 template<typename T, size_t partitions, size_t page_size = 5 * 1024 * 1024>
 void process_morsel_smb(MorselCreator<T> &morsel_creator, OnDemandPageManager<T, partitions, page_size> &page_manager) {
-    thread_local std::shared_ptr<T[]> buffer;
-    thread_local std::array<unsigned, partitions> buffer_index = {0};
-    const auto buffer_size = page_size / sizeof(T) >> 1;
-    // std::cout << "Buffer size in KB: " << buffer_size * sizeof(T) / 1024 << " and " << buffer_size / sizeof(T) << " Tuples per partition"  <<  std::endl;
-    const auto total_buffer_size = buffer_size * partitions;
-
-    if (!buffer) {
-        buffer = std::make_shared<T[]>(total_buffer_size);
-    }
+    std::array<unsigned, partitions> buffer_index = {0};
+    const auto total_buffer_size = 4 * 1024 * 1024 / sizeof(T);
+    const auto buffer_size_per_partition = total_buffer_size / partitions;
+    std::shared_ptr<T[]> buffer = std::make_shared<T[]>(total_buffer_size);
 
     for (auto [batch, batch_size] = morsel_creator.getBatchOfTuples(); batch; std::tie(batch, batch_size) = morsel_creator.getBatchOfTuples()) {
         for (size_t i = 0; i < batch_size; ++i) {
             auto tuple = batch[i];
             auto partition = partition_function<T, partitions>(tuple);
             auto &index = buffer_index[partition];
-            auto partition_offset = partition * buffer_size;
+            auto partition_offset = partition * buffer_size_per_partition;
 
-            if (index == buffer_size) {
-                page_manager.insert_buffer_of_tuples(buffer.get() + partition_offset, buffer_size, partition);
+            if (index == buffer_size_per_partition) {
+                page_manager.insert_buffer_of_tuples(buffer.get() + partition_offset, buffer_size_per_partition, partition);
                 index = 0;
             }
 
@@ -32,7 +27,7 @@ void process_morsel_smb(MorselCreator<T> &morsel_creator, OnDemandPageManager<T,
     }
 
     for (unsigned i = 0; i < partitions; ++i) {
-        auto partition_offset = i * buffer_size;
+        auto partition_offset = i * buffer_size_per_partition;
         if (buffer_index[i] > 0) {
             page_manager.insert_buffer_of_tuples(buffer.get() + partition_offset, buffer_index[i], i);
         }
