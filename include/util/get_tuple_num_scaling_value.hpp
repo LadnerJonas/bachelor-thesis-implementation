6e9fb64c6204c9cc7cff18d2a0992bb4a5d00abd
LadnerJonas <jonas.ladner+git@tum.de>
2024-11-13 14:35:08 +0100

    adapt get_tuple_num_scaling_value for 1024 partitions with 75% load factor

--- include/util/get_tuple_num_scaling_value.hpp
+++ include/util/get_tuple_num_scaling_value.hpp
@@ -8,13 +8,13 @@ class Tuple100;
 template<typename T>
 double get_tuple_num_scaling_value() {
     if (std::is_same_v<T, Tuple100>) {
-        return 1.50;
+        return 1.87;
     }
     if (std::is_same_v<T, Tuple16>) {
-        return 6;
+        return 8.4;
     }
     if (std::is_same_v<T, Tuple4>) {
-        return 14.001;
+        return 16.8;
     }
     return 1;
 }
