4dd67deb6b343326ac649d5a797ad7dd6aec57e8
LadnerJonas <jonas.ladner+git@tum.de>
2024-11-14 10:21:05 +0100

    fix ContinuousMaterialization benchmark bugs

--- benchmark/materialization/benchmark.cpp
+++ benchmark/materialization/benchmark.cpp
@@ -16,9 +16,9 @@ void benchmark_materialization(size_t tuples_to_generate) {
 constexpr size_t batch_sizes[] = {8, 32, 512, 2048};
 int main() {
     BenchmarkParameters params;
-    for (unsigned tuples_to_generate = 40'000'000u; tuples_to_generate <= 40'000'000u; tuples_to_generate *= 10) {
+    for (unsigned tuples_to_generate_base = 40'000'000u; tuples_to_generate_base <= 40'000'000u; tuples_to_generate_base *= 10) {
         params.setParam("A-Benchmark tuple-materialization", "ContinuousMaterialization");
-        tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate) * get_tuple_num_scaling_value<Tuple16>());
+        auto tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate_base) * get_tuple_num_scaling_value<Tuple16>());
         params.setParam("B-Tuples", tuples_to_generate);
         params.setParam("C-batch_size", sizeof(Tuple16));
         {
@@ -43,7 +43,7 @@ int main() {
             benchmark_materialization<Tuple16, batch_sizes[3]>(tuples_to_generate);
         }
 
-        tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate) * get_tuple_num_scaling_value<Tuple100>());
+        tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate_base) * get_tuple_num_scaling_value<Tuple100>());
         params.setParam("B-Tuples", tuples_to_generate);
         params.setParam("C-batch_size", sizeof(Tuple100));
         {
@@ -67,29 +67,29 @@ int main() {
             PerfEventBlock e(1'000'000, params, false);
             benchmark_materialization<Tuple100, batch_sizes[3]>(tuples_to_generate);
         }
-        tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate) * get_tuple_num_scaling_value<Tuple4>());
+        tuples_to_generate = static_cast<unsigned>(static_cast<double>(tuples_to_generate_base) * get_tuple_num_scaling_value<Tuple4>());
         params.setParam("B-Tuples", tuples_to_generate);
         params.setParam("C-batch_size", sizeof(Tuple4));
         {
             params.setParam("D-batch_size", batch_sizes[0]);
             PerfEventBlock e(1'000'000, params, true);
-            benchmark_materialization<Tuple100, batch_sizes[0]>(tuples_to_generate);
+            benchmark_materialization<Tuple4, batch_sizes[0]>(tuples_to_generate);
         }
 
         {
             params.setParam("D-batch_size", batch_sizes[1]);
             PerfEventBlock e(1'000'000, params, false);
-            benchmark_materialization<Tuple100, batch_sizes[1]>(tuples_to_generate);
+            benchmark_materialization<Tuple4, batch_sizes[1]>(tuples_to_generate);
         }
         {
             params.setParam("D-batch_size", batch_sizes[2]);
             PerfEventBlock e(1'000'000, params, false);
-            benchmark_materialization<Tuple100, batch_sizes[2]>(tuples_to_generate);
+            benchmark_materialization<Tuple4, batch_sizes[2]>(tuples_to_generate);
         }
         {
             params.setParam("D-batch_size", batch_sizes[3]);
             PerfEventBlock e(1'000'000, params, false);
-            benchmark_materialization<Tuple100, batch_sizes[3]>(tuples_to_generate);
+            benchmark_materialization<Tuple4, batch_sizes[3]>(tuples_to_generate);
         }
     }
     return 0;
