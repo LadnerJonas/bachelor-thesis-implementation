7facc71cb6762d60cc320f7cefffd19a82fba4f5
LadnerJonas <jonas.ladner+git@tum.de>
2024-11-17 21:21:06 +0100

    smaller fixes to adapt the recent changes

--- profile_data/rename-profile-files.sh
+++ profile_data/rename-profile-files.sh
@@ -4,12 +4,13 @@
 TARGET_DIR="profile_data"
 
 # Find and rename all files with "cmake-build-pgo_instrument" in the filename
-find "$TARGET_DIR" -type f -name "*cmake-build-pgo_instrument*" | while read -r file; do
+find "$TARGET_DIR" -type f -name "*cmake-build-pgo_instrument*" | while IFS= read -r file; do
     # Create the new filename by replacing "cmake-build-pgo_instrument" with "cmake-build-pgo_optimize"
     new_file="${file//cmake-build-pgo_instrument/cmake-build-pgo_optimize}"
     
     # Rename the file
     mv "$file" "$new_file"
+    echo "Renamed $file to $new_file"
 done
 
 echo "Renaming complete. All files have been updated."
