4ed91ccb67776e61642c3d1c159b374a513aea03
LadnerJonas <jonas.ladner+git@tum.de>
2024-12-04 14:13:15 +0100

    add get_multiple_pages and PADDING

--- include/slotted-page/page-pool/SlottedPagePool.hpp
+++ include/slotted-page/page-pool/SlottedPagePool.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include "slotted-page/page-implementation/RawSlottedPage.hpp"
 
-template<typename T, size_t partitions, size_t page_size = 5 * 1024 * 1024>
+constexpr auto PADDING = 4096u;
+
+template<size_t page_size = 5 * 1024 * 1024>
 class SlottedPagePool {
     std::shared_ptr<uint8_t[]> page_data;
     std::atomic<unsigned> current_page = 0;
@@ -9,17 +10,20 @@ class SlottedPagePool {
 
 public:
     SlottedPagePool() = default;
-    explicit SlottedPagePool(const size_t tuples_to_store) {
-        const auto tuples_per_page = RawSlottedPage<T>::get_max_tuples(page_size);
-        const auto tuples_per_partition = (tuples_to_store + partitions - 1) / partitions;
-        const auto pages = partitions * ((tuples_per_partition + tuples_per_page - 1) / tuples_per_page);
+    explicit SlottedPagePool(const size_t pages) {
         max_pages = pages;
-        page_data = std::make_shared<uint8_t[]>(pages * page_size);
+        page_data = std::make_shared<uint8_t[]>(pages * (page_size + PADDING));
     }
 
-    auto get_single_page() {
+    uint8_t *get_single_page() {
         assert(current_page < max_pages);
-        return page_data.get() + current_page++ * page_size;
+        return page_data.get() + current_page++ * (page_size + PADDING);
+    }
+
+    uint8_t *get_multiple_pages(const size_t num_pages) {
+        assert(current_page + num_pages <= max_pages);
+        auto start = current_page.fetch_add(num_pages);
+        return page_data.get() + start * (page_size + PADDING);
     }
     auto has_free_page() const {
         return current_page < max_pages;
