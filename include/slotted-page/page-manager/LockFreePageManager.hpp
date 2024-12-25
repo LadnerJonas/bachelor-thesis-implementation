dff06c945f92a9486721ae77e51de4900b71e539
LadnerJonas <jonas.ladner+git@tum.de>
2024-12-25 16:39:17 +0100

    replace shared_ptr with raw_ptr

--- include/slotted-page/page-manager/LockFreePageManager.hpp
+++ include/slotted-page/page-manager/LockFreePageManager.hpp
@@ -3,22 +3,31 @@
 #include "slotted-page/page-implementation/LockFreeManagedSlottedPage.hpp"
 #include "util/padded/PaddedAtomic.hpp"
 #include <array>
+#include <vector>
 
 template<typename T, size_t partitions, size_t page_size = 5 * 1024 * 1024>
 class LockFreePageManager {
-    std::array<std::vector<std::shared_ptr<LockFreeManagedSlottedPage<T>>>, partitions> pages{};
-    std::array<PaddedAtomic<std::shared_ptr<LockFreeManagedSlottedPage<T>>>, partitions> current_pages{};
+    std::array<std::vector<LockFreeManagedSlottedPage<T> *>, partitions> pages;
+    std::array<PaddedAtomic<LockFreeManagedSlottedPage<T> *>, partitions> current_pages;
 
     void add_page(unsigned partition) {
-        pages[partition].emplace_back(std::make_shared<LockFreeManagedSlottedPage<T>>(page_size));
-        current_pages[partition].store(pages[partition].back());
+        auto new_page = new LockFreeManagedSlottedPage<T>(page_size);
+        pages[partition].emplace_back(new_page);
+        current_pages[partition].store(new_page);
     }
-
 
 public:
     LockFreePageManager() {
         for (unsigned i = 0; i < partitions; ++i) {
             add_page(i);
+        }
+    }
+
+    ~LockFreePageManager() {
+        for (unsigned i = 0; i < partitions; ++i) {
+            for (auto &page: pages[i]) {
+                delete page;
+            }
         }
     }
 
@@ -34,20 +43,19 @@ public:
     }
 
     void insert_buffer_of_tuples(const T *buffer, const size_t num_tuples, const size_t partition) {
-        for (unsigned i = 0; i < num_tuples; i++) {
-            const auto &tuple = buffer[i];
-            insert_tuple(tuple, partition);
+        for (size_t i = 0; i < num_tuples; i++) {
+            insert_tuple(buffer[i], partition);
         }
     }
 
     void insert_buffer_of_tuples_batched(const T *buffer, const size_t num_tuples, const size_t partition) {
         auto tuples_left = num_tuples;
         while (tuples_left > 0) {
-            typename LockFreeManagedSlottedPage<T>::BatchedWriteInfo wi;
-            do {
+            auto wi = current_pages[partition].load()->increment_and_fetch_opt_write_info(tuples_left);
+            while (wi.page_data == nullptr) {
                 wi = current_pages[partition].load()->increment_and_fetch_opt_write_info(tuples_left);
-            } while (wi.page_data == nullptr);
-            if (wi.tuple_index + wi.tuples_to_write >= LockFreeManagedSlottedPage<T>::get_max_tuples(page_size) - 1) {
+            }
+            if (wi.tuples_to_write < tuples_left || wi.tuple_index + wi.tuples_to_write >= LockFreeManagedSlottedPage<T>::get_max_tuples(page_size) - 1) {
                 add_page(partition);
             }
             LockFreeManagedSlottedPage<T>::add_batch_using_index(buffer + num_tuples - tuples_left, wi);
