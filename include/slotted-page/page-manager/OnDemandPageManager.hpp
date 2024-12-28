5012d711426a12b1897135ccc6eb8bb3cc91970e
LadnerJonas <jonas.ladner+git@tum.de>
2024-12-28 16:44:33 +0100

    improve performance

--- include/slotted-page/page-manager/OnDemandPageManager.hpp
+++ include/slotted-page/page-manager/OnDemandPageManager.hpp
@@ -3,11 +3,12 @@
 #include "slotted-page/page-implementation/ManagedSlottedPage.hpp"
 #include "util/padded/PaddedMutex.hpp"
 #include <array>
+#include <deque>
 
 template<typename T, size_t partitions, size_t page_size = 5 * 1024 * 1024>
 class OnDemandPageManager {
     std::array<PaddedMutex, partitions> partition_locks;
-    std::array<std::vector<ManagedSlottedPage<T>>, partitions> pages;
+    std::array<std::deque<ManagedSlottedPage<T>>, partitions> pages;
 
 public:
     OnDemandPageManager() {
@@ -17,7 +18,7 @@ public:
     }
 
     void insert_tuple(const T &tuple, size_t partition) {
-        std::lock_guard lock(partition_locks[partition].mutex);
+        std::lock_guard lock(partition_locks[partition]);
         if (!pages[partition].back().add_tuple(tuple)) {
             pages[partition].emplace_back(page_size);
             pages[partition].back().add_tuple(tuple);
@@ -25,7 +26,7 @@ public:
     }
 
     void insert_buffer_of_tuples(const T *buffer, const size_t num_tuples, const size_t partition) {
-        std::lock_guard lock(partition_locks[partition].mutex);
+        std::lock_guard lock(partition_locks[partition]);
         for (unsigned i = 0; i < num_tuples; i++) {
             const auto &tuple = buffer[i];
             if (!pages[partition].back().add_tuple(tuple)) {
@@ -36,22 +37,24 @@ public:
     }
 
     void insert_buffer_of_tuples_batched(const T *buffer, const size_t num_tuples, const size_t partition) {
-        unsigned tuples_left = num_tuples;
+        unsigned tuples_left = num_tuples, tuples_to_write = 0, index;
+        ManagedSlottedPage<T> *current_page;
         {
-            std::lock_guard lock(partition_locks[partition].mutex);
-            auto &current_page = pages[partition].back();
-            const auto index = current_page.get_tuple_count();
-            auto tuples_left_on_page = ManagedSlottedPage<T>::get_max_tuples(page_size) - index;
+            std::lock_guard lock(partition_locks[partition]);
+            current_page = &pages[partition].back();
+            index = current_page->get_tuple_count();
+            const auto tuples_left_on_page = ManagedSlottedPage<T>::get_max_tuples(page_size) - index;
             if (tuples_left_on_page == 0) {
                 pages[partition].emplace_back(page_size);
             } else {
                 tuples_left = num_tuples - std::min(tuples_left_on_page, num_tuples);
-                auto const tuples_to_write = num_tuples - tuples_left;
-                current_page.increase_tuple_count(tuples_to_write);
-                current_page.add_tuple_batch_with_index(buffer, index, tuples_to_write);
+                tuples_to_write = num_tuples - tuples_left;
+                current_page->increase_tuple_count(tuples_to_write);
             }
         }
-
+        if (tuples_to_write > 0) {
+            current_page->add_tuple_batch_with_index(buffer, index, tuples_to_write);
+        }
         if (tuples_left > 0) {
             insert_buffer_of_tuples_batched(buffer + num_tuples - tuples_left, tuples_left, partition);
         }
